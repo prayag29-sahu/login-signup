5425570d3d60cabf20cfe68ba5e09191ca496a73
Prayag Sahu <sahuprayag229@gmail.com>
2025-12-17 20:50:01 +0530

    updation

--- Backend_5thSem/twillioService.js
+++ Backend_5thSem/twillioService.js
@@ -1,17 +1,18 @@
 // twilioService.js
 const twilio = require('twilio');
 
-const accountSid = 'ACfeac8cb3e7dae0fcd683e278'; // Your Account SID from Twilio Console
-const authToken = '3fa8f2fccfa7f27f4a267e910505';   // Your Auth Token from Twilio Console
+const accountSid = 'ACfeac8cb3sgcjhs8fcd683e278'; // dummy id 
+const authToken = '3fa8fccfa746427f4a267e910505';   // dummy Auth Token 
 
 const client = new twilio(accountSid, authToken);
 
 const sendOtp = (phoneNumber, otp) => {
   return client.messages.create({
     body: `Your OTP is ${otp}`,  // The message content
-    from: '+123146657632', // Your Twilio Phone Number
+    from: '+123966657632', // Your Twilio Phone Number
     to: phoneNumber                // Recipient's Phone Number
   });
 };
+
 
 module.exports = sendOtp;
